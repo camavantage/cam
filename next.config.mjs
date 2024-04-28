800086e484fbd843b72ab68badba912092e8d18d
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-04-28 11:37:25 +0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            hostname: '**',
+          },
+        ],
+        formats: ['image/webp'],
+        
+      },
+};
 
 export default nextConfig;
