9b276e133508d960ab49f1f97c3a774e8e492fdb
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-04-28 22:18:52 +0300

    deplousdaf

--- src/components/home/sections/aboutSection.tsx
+++ src/components/home/sections/aboutSection.tsx
@@ -8,18 +8,20 @@ import Link from "next/link";
 
 const AboutSection: React.FC = () => {
   return (
-    <div className={`max-w-5xl mx-auto py-12 md:py-20 px-4`}>
-      <div
-        // className={`p-4 md:p-8 text-sm md:text-base ${s.about} flex flex-col items-center rounded-3xl`}
-      >
+    <div className={`max-w-5xl mx-auto`}>
+       {/* py-12 md:py-20 px-4 */}
+      {/* <div
+        className={`p-4 md:p-8 text-sm md:text-base ${s.about} flex flex-col items-center rounded-3xl`}
+      > */}
         {/* <div className="flex justify-center items-center border-2 border-cam-green-800 rounded-full"> */}
-          <LogoWithtext className="h-14 w-14 " />
+          
         {/* </div> */}
         <PageHeader>
-          <PageHeaderHeading>Une école reconnue</PageHeaderHeading>
-          <PageHeaderDescription>CAM/AVANTAGE est un établissement privé déclaré au ministère de la
+        <LogoWithtext className="h-20 w-20 " />
+          <PageHeaderHeading>Qui sommes-nous?</PageHeaderHeading>
+          <PageHeaderDescription>Nous sommes un centre d&apos;apprentisage et des Métiers. Un établissement privé déclaré au ministère de la
           formation professionnelle. Partenaire Officiel du groupe des centres
-          professionnels en RDC certification de mérite.</PageHeaderDescription>
+          professionnels en République démocratique du Congo.</PageHeaderDescription>
           <PageActions>
             <Link href="/about">
             <Button className="rounded-full">En savoir plus</Button>
@@ -33,7 +35,7 @@ const AboutSection: React.FC = () => {
           formation professionnelle. Partenaire Officiel du groupe des centres
           professionnels en RDC certification de mérite.
         </p> */}
-      </div>
+      {/* </div> */}
     </div>
   );
 };
