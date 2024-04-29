f242dbc56a6924b3516ff09d0951ff70760167f1
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-04-29 09:46:31 +0300

    deploy changess

--- src/app/(frontoffice)/courses/page.tsx
+++ src/app/(frontoffice)/courses/page.tsx
@@ -1,3 +1,21 @@
-export default function CoursesPage(){
-    return <div></div>
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header";
+
+export default function CoursesPage() {
+  return (
+    <div className="max-w-screen-lg mx-auto px-6 lg:px-0">
+      <PageHeader>
+        <PageHeaderHeading>Les Formations adaptées</PageHeaderHeading>
+        <PageHeaderDescription>
+          Apprenez un métier essentiel à votre rythme avec nos formations en
+          ligne accessibles partout et tout le temps ou directement dans nos
+          locaux. Vous pouvez suivre une formation en apprentissage 100% théorie
+          en ligne et 100% pratique en entreprise
+        </PageHeaderDescription>
+      </PageHeader>
+    </div>
+  );
 }
