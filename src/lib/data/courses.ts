a698bc5bd7b9885166b03cb1d9e918819dc45e79
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-03 15:35:30 +0300

    deploy chnages

---
+++ src/lib/data/courses.ts
@@ -0,0 +1,62 @@
+export const courses = [
+    {
+      id: "1",
+      name: "Pâtisserie",
+      description: "",
+      mediaItem: {
+        sourceUrl: "/assets/images/course-illustrations/fouet.png",
+        alt: "Fouet pâtesserie illustration",
+        name: "",
+      },
+    },
+    {
+      id: "2",
+      name: "Boulangerie",
+      description: "",
+      mediaItem: {
+        sourceUrl: "/assets/images/course-illustrations/boulanger.png",
+        alt: "Boulanger illustration",
+        name: "",
+      },
+    },
+    {
+      id: "3",
+      name: "Charcuterie",
+      description: "",
+      mediaItem: {
+        sourceUrl: "/assets/images/course-illustrations/saucisson.png",
+        alt: "Charcuterie avantage illustration",
+        name: "",
+      },
+    },
+    {
+      id: "4",
+      name: "Confiserie",
+      description: "",
+      mediaItem: {
+        sourceUrl: "/assets/images/course-illustrations/jus-de-fruit.png",
+        alt: "Jus de fruits avantage illustration",
+        name: "",
+      },
+    },
+    {
+      id: "5",
+      name: "Hôtellerie",
+      description: "",
+      mediaItem: {
+        sourceUrl: "/assets/images/course-illustrations/des-lits.png",
+        alt: "lits d'hotel avantage illustration",
+        name: "",
+      },
+    },
+    {
+      id: "6",
+      name: "Restaurantion",
+      description: "",
+      mediaItem: {
+        sourceUrl: "/assets/images/course-illustrations/serveur.png",
+        alt: "Serveur avantage illustration",
+        name: "",
+      },
+    },
+  ];
