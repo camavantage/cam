b77c6b973aa9d4dcefb9992a8bbf3c59dfc4b97c
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-06 21:04:54 +0300

    fix

--- src/components/no-subscriber-message.tsx
+++ src/components/no-subscriber-message.tsx
@@ -1,10 +1,11 @@
-import { PageActions, PageHeader, PageHeaderDescription } from "./page-header";
+import { PageActions, PageHeader, PageHeaderDescription, PageHeaderHeading } from "./page-header";
 import { Button } from "./ui/button";
 
 export function NoSubscriberMessage() {
   return (
     <div>
       <PageHeader>
+        <PageHeaderHeading>S&apos;inscrire d&apos;abord</PageHeaderHeading>
         <PageHeaderDescription>
           Cet article n&apos;est visible que pour les abonnés de
           cam-avantage.com. Pour ne rien rater, connectez-vous à votre compte ou
