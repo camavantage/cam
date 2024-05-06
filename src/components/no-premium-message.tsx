f41a1b052e149d4f31c0b2408a73e3ff44bc9212
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-06 21:48:51 +0300

    deploy changes

--- src/components/no-premium-message.tsx
+++ src/components/no-premium-message.tsx
@@ -10,7 +10,7 @@ export function NoPremiumMessage() {
   return (
     <div>
       <PageHeader>
-        <PageHeaderHeading>Accès à 1$</PageHeaderHeading>
+        <PageHeaderHeading>c&apos;est article est payant</PageHeaderHeading>
         <PageHeaderDescription>
           Seuls les abonnés qui ont payés pour cet article peuvent le consulter.
         </PageHeaderDescription>
