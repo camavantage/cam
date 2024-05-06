b2e85c180b56dfc58b24893e38d9aaaa4fc125d1
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-06 22:01:37 +0300

    fix

--- src/components/no-premium-message.tsx
+++ src/components/no-premium-message.tsx
@@ -10,9 +10,11 @@ export function NoPremiumMessage() {
   return (
     <div>
       <PageHeader>
-        <PageHeaderHeading>c&apos;est article est payant</PageHeaderHeading>
+        <PageHeaderHeading>Accès payant</PageHeaderHeading>
         <PageHeaderDescription>
           Seuls les abonnés qui ont payés pour cet article peuvent le consulter.
+          Les articles payants sont des formations et guides pratiques avec
+          accompagnement.
         </PageHeaderDescription>
         <PageActions>
           <Button className="rounded-full">Acheter maintenant</Button>
