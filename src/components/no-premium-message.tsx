81b13858d33a9ed36700fbbc234195ec094bb1b0
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-06 22:06:59 +0300

    fix

--- src/components/no-premium-message.tsx
+++ src/components/no-premium-message.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   PageActions,
   PageHeader,
@@ -10,14 +11,20 @@ export function NoPremiumMessage() {
   return (
     <div>
       <PageHeader>
+      <Link
+            href=""
+            className="inline-flex items-center rounded-full bg-muted px-3 py-1 text-sm font-medium"
+          >
+            <span>1$</span>
+          </Link>
         <PageHeaderHeading>Accès payant</PageHeaderHeading>
         <PageHeaderDescription>
           Seuls les abonnés qui ont payés pour cet article peuvent le consulter.
           Les articles payants sont des formations et guides pratiques avec
-          accompagnement.
+          accompagnement garantie.
         </PageHeaderDescription>
         <PageActions>
-          <Button className="rounded-full">Acheter maintenant</Button>
+          <Button className="rounded-full">Acheter maintenant pour 1$</Button>
         </PageActions>
       </PageHeader>
     </div>
