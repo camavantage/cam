32bac70746f6fd9012908cf35477f3fa2fcacef7
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-07 15:25:19 +0300

    fix

--- src/lib/zod/articles/index.ts
+++ src/lib/zod/articles/index.ts
@@ -3,35 +3,40 @@ import { tagOptionSchema } from "@/lib/utils";
 import { z } from "zod";
 
 // new article form data schema
-export const newArticleFormSchema = z.object({
-  title: z
-    .string({ required_error: "Le titre est obligatoire" })
-    .min(2, {
-      message: "Le titre doit comporter au moins 2 caractères.",
-    })
-    .trim(),
-  description: z
-    .string()
-    .max(255, {
-      message: "La description doit comporter au max 255 caractères.",
-    })
-    .optional(),
-  content: z.string(),
-  markdown: z.string(),
-  imageUrl: z.string(),
-  tags: z.array(tagOptionSchema),
-  customTags: z.array(tagOptionSchema),
-  authorId: z.string().cuid(),
-  published: z.boolean(),
-  commentable: z.boolean(),
-  visibility: z.enum(visibilities).optional(),
-  price: z.number().optional(),
-  verified: z.boolean(),
-  blocked: z.boolean(),
-}).refine((data) => data.visibility === "premium_only"&& typeof data.price==="undefined", {
-  path: ["price"],
-  message: "Obligatoire",
-});
+export const newArticleFormSchema = z
+  .object({
+    title: z
+      .string({ required_error: "Le titre est obligatoire" })
+      .min(2, {
+        message: "Le titre doit comporter au moins 2 caractères.",
+      })
+      .trim(),
+    description: z
+      .string()
+      .max(255, {
+        message: "La description doit comporter au max 255 caractères.",
+      })
+      .optional(),
+    content: z.string(),
+    markdown: z.string(),
+    imageUrl: z.string(),
+    tags: z.array(tagOptionSchema),
+    customTags: z.array(tagOptionSchema),
+    authorId: z.string().cuid(),
+    published: z.boolean(),
+    commentable: z.boolean(),
+    visibility: z.enum(visibilities).optional(),
+    price: z.number().optional(),
+    verified: z.boolean(),
+    blocked: z.boolean(),
+  })
+  .refine(
+    (data) => (data.visibility === "premium_only" ? Boolean(data.price) : true),
+    {
+      path: ["price"],
+      message: "Obligatoire",
+    }
+  );
 // new article form data schema as a type
 export type NewArticleFormSchemaType = z.infer<typeof newArticleFormSchema>;
 
