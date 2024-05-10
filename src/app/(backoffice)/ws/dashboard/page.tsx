7c030d32044f876557a3ca5bd64a04a8d8ad1e84
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-10 16:19:43 +0300

    fix

--- src/app/(backoffice)/ws/dashboard/page.tsx
+++ src/app/(backoffice)/ws/dashboard/page.tsx
@@ -161,7 +161,7 @@ export default async function WSDashboardPage() {
                         {orders.map((order) => (
                           <div
                             key={order.articleId + order.client}
-                            className="flex space-x-3 py-3 border-b"
+                            className="flex space-x-4 py-4 border-b"
                           >
                             <Avatar>
                               <AvatarImage src={order.client?.image ?? ""} />
