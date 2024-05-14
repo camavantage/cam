56cc570e2331cc7625f1fc9076ef83e3443b601f
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-14 22:54:09 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={josefinSans.className}>
-      <NextTopLoader color="#ffffff" showSpinner={false} />
+      <NextTopLoader color="#B6D300" showSpinner={false} />
         <Providers>
           {children}
           <Toaster />
