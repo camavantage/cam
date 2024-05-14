4ef838bfe74841f6a9271a9e70f190e7f9e4579e
Kahindo Lwanzo Alfred <89693650+alfredlwanzo@users.noreply.github.com>
2024-05-14 14:34:12 +0300

    fix

--- src/app/(frontoffice)/layout.tsx
+++ src/app/(frontoffice)/layout.tsx
@@ -1,14 +1,45 @@
+import { getAppSetup } from "@/actions/ws/setup";
 import { SiteFooter } from "@/components/footer";
 import { SiteHeader } from "@/components/header";
 import { NewsletterSection } from "@/components/newsletter";
+import { PageHeader, PageHeaderDescription, PageHeaderHeading } from "@/components/page-header";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { appStatusDescription } from "@/lib/utils";
 import React, { Suspense } from "react";
 
-export default function FrontOfficeLayout({
+export default async function FrontOfficeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const app = await getAppSetup();
+
+  if (app?.status === "offline") {
+    return (
+      <div className="pt-24 min-h-screen">
+        <PageHeader>
+          <PageHeaderHeading>Hors ligne</PageHeaderHeading>
+          <PageHeaderDescription>
+            {appStatusDescription(app.status)}
+          </PageHeaderDescription>
+        </PageHeader>
+      </div>
+    );
+  }
+
+  if (app?.status === "maintenance") {
+    return (
+      <div className="pt-24 min-h-screen">
+        <PageHeader>
+        <PageHeaderHeading>En maintenance</PageHeaderHeading>
+          <PageHeaderDescription>
+            {appStatusDescription(app.status)}
+          </PageHeaderDescription>
+        </PageHeader>
+      </div>
+    );
+  }
+
   return (
     <ScrollArea className="h-screen">
       <SiteHeader />
@@ -18,7 +49,7 @@ export default function FrontOfficeLayout({
           <NewsletterSection />
         </Suspense>
       </section>
-      <SiteFooter/>
+      <SiteFooter />
     </ScrollArea>
   );
 }
